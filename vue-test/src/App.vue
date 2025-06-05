c5cb6a57b19355d0388253da4b6fe3d009c98219
Johannes Danielson <hannes16@live.se>
2025-06-05 09:24:34 +0200

    Initiering

---
+++ vue-test/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: 'Kalle med megaballe',
+        status :  'pending',
+        tasks: ['Task 1', 'Task 2', 'Task 3']
+      }
+    }
+  }
+</script>
